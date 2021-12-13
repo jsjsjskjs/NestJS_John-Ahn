bf97fc6c14766117e8807821af8e32fdaf77da53
jsjsjskjs <jsjsjskjs@gmail.com>
2021-12-13 20:54:57 +0900

    CRUD_모든 데이터 가져오기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private boards = [] // 다른 컴포넌트에서 수정할 수 없도록 private 사용
+
+    getAllBoards() {
+        return this.boards
+    }
+}
