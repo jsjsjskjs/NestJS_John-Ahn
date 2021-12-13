dc14c877af57a0907c08cb7cbbaa9cd0f12a2612
jsjsjskjs <jsjsjskjs@gmail.com>
2021-12-13 21:03:11 +0900

    CRUD 모델 정의하기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-    private boards = [] // 다른 컴포넌트에서 수정할 수 없도록 private 사용
+    private boards: Board[] = [] // 다른 컴포넌트에서 수정할 수 없도록 private 사용
 
-    getAllBoards() {
+    getAllBoards(): Board[] {
         return this.boards
     }
 }
