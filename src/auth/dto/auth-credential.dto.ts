ca6b91ee2d260150110c9f0a240d1d73f51cde3c
jsjsjskjs <jsjsjskjs@gmail.com>
2021-12-29 16:50:48 +0900

    유니크 값 만들기

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,18 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator"
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    // 영어와 숫자만 가능하도록 정규 표현식 사용
+    // 두번째 인자에는 유효성에 맞지 않는 경우 메시지 전달이 가능
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: '비밀번호는 오직 영어와 숫자만 가능합니다'
+    })
     password: string
 }
