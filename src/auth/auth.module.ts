214452ab73885a32760ba5f21173bb9aafd76e58
jsjsjskjs <jsjsjskjs@gmail.com>
2021-12-31 23:53:32 +0900

    jwt 모듈 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -6,6 +7,12 @@ import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: 'secret1234',
+      signOptions:{
+        expiresIn: 60 * 60
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
