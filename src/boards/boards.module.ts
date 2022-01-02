6fd8ec05b7ec1fdb807ca558c81b0a2e064d46de
jsjsjskjs <jsjsjskjs@gmail.com>
2022-01-03 00:42:23 +0900

    인증된 유저만 게시글 접근

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
@@ -7,6 +8,7 @@ import { BoardsService } from './boards.service';
 @Module({
   imports: [
     TypeOrmModule.forFeature([BoardRepository]),
+    AuthModule // UseGuards를 사용하기 위해서 auth.module에 있는 AuthModule을 가져온다
   ],
   controllers: [BoardsController],
   providers: [BoardsService]
